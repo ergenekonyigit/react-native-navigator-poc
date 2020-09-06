a667da79c43a7d6b877d9f93285b52093c374013
ergenekonyigit <ergenekon.yigit@trendyol.com>
2020-09-06 23:01:04 +0300

    fix

--- src/SecondScreen.js
+++ src/SecondScreen.js
@@ -1,13 +1,12 @@
 import React from "react";
 import { View, Text } from "react-native";
 
-
 function SecondScreen() {
   return (
-    <View style={{ flex: 1 justifyContent: 'center' }}>
+    <View style={{ flex: 1, justifyContent: "center" }}>
       <Text>second screen</Text>
     </View>
-  )
+  );
 }
 
 export default SecondScreen;
