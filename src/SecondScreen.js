7dc4d262f0ca7ea06448b8dab410aea3b46f0eb9
ergenekonyigit <ergenekon.yigit@trendyol.com>
2020-09-06 22:59:11 +0300

    screens

---
+++ src/SecondScreen.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+
+function SecondScreen() {
+  return (
+    <View style={{ flex: 1 justifyContent: 'center' }}>
+      <Text>second screen</Text>
+    </View>
+  )
+}
+
+export default SecondScreen;
