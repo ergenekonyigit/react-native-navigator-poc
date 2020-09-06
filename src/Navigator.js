026cfa330e9c94eb392c65fd93f7be735deb5431
ergenekonyigit <ergenekon.yigit@trendyol.com>
2020-09-06 23:23:54 +0300

    fix

--- src/Navigator.js
+++ src/Navigator.js
@@ -27,52 +27,51 @@ export default function Navigator({ navigation }) {
   return (
     <>
       <StatusBar backgroundColor="#273142" barStyle="light-content" />
-      <SafeAreaView style={{ flex: 0, backgroundColor: "#273142" }}>
-        <Tab.Navigator
-          initialRouteName="FirstScreen"
-          tabBarOptions={{
-            activeTintColor: "#f27a1a",
-            inactiveTintColor: "#6E7787",
-            activeBackgroundColor: "#ffffff",
-            inactiveBackgroundColor: "#ffffff",
-            labelStyle: {
-              fontWeight: "bold",
+      <SafeAreaView style={{ flex: 0, backgroundColor: "#273142" }} />
+      <Tab.Navigator
+        initialRouteName="FirstScreen"
+        tabBarOptions={{
+          activeTintColor: "#f27a1a",
+          inactiveTintColor: "#6E7787",
+          activeBackgroundColor: "#ffffff",
+          inactiveBackgroundColor: "#ffffff",
+          labelStyle: {
+            fontWeight: "bold",
+          },
+          style: {
+            elevation: 5,
+            shadowColor: "#273142",
+            shadowOffset: {
+              width: 0,
+              height: -4,
             },
-            style: {
-              elevation: 5,
-              shadowColor: "#273142",
-              shadowOffset: {
-                width: 0,
-                height: -4,
-              },
-              shadowRadius: 15,
-              shadowOpacity: 0.1,
-              borderColor: "#273142",
-            },
+            shadowRadius: 15,
+            shadowOpacity: 0.1,
+            borderColor: "#273142",
+          },
+        }}
+      >
+        <Tab.Screen
+          name="FirstScreen"
+          component={FirstScreen}
+          options={{
+            title: "FIRST",
+            tabBarIcon: ({ focused }) => (
+              <FirstIco color={focused ? "#f27a1a" : "#6E7787"} />
+            ),
           }}
-        >
-          <Tab.Screen
-            name="FirstScreen"
-            component={FirstScreen}
-            options={{
-              title: "FIRST",
-              tabBarIcon: ({ focused }) => (
-                <FirstIco color={focused ? "#f27a1a" : "#6E7787"} />
-              ),
-            }}
-          />
-          <Tab.Screen
-            name="SecondScreen"
-            component={SecondScreen}
-            options={{
-              title: "SECOND",
-              tabBarIcon: ({ focused }) => (
-                <SecondIco color={focused ? "#f27a1a" : "#6E7787"} />
-              ),
-            }}
-          />
-        </Tab.Navigator>
-      </SafeAreaView>
+        />
+        <Tab.Screen
+          name="SecondScreen"
+          component={SecondScreen}
+          options={{
+            title: "SECOND",
+            tabBarIcon: ({ focused }) => (
+              <SecondIco color={focused ? "#f27a1a" : "#6E7787"} />
+            ),
+          }}
+        />
+      </Tab.Navigator>
     </>
   );
 }
